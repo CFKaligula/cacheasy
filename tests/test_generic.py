4e74ccffa561705bcd81c0f09792031f21e9cf5f
Casper Kole <casper@voltgoed.nl>
2024-07-17 17:05:06 +0200

    add tests

--- tests/test_generic.py
+++ tests/test_generic.py
@@ -1,6 +1,7 @@
-from src import cacheasy as local_cacheasy
+from src import cacheasy
 import os
 import pytest
+import shutil
 
 
 @pytest.fixture
@@ -10,16 +11,57 @@ def env_file_path():
     os.remove(env_file_path)
 
 
+@pytest.fixture()
+def my_cool_x_cached_file_name():
+    x = 5
+    cache_file_path = 'my_cool_x'
+    cacheasy.write_to_cache(x, cache_file_path)
+    yield cache_file_path
+    cacheasy.remove_from_cache(cache_file_path)
+
+
 def test_cache_folder():
-    assert local_cacheasy.get_cache_folderpath() == 'cache'
+    assert cacheasy.get_cache_folderpath() == 'cache'
 
 
 def test_cache_folder_with_env(env_file_path):
 
     with open(env_file_path, 'w') as env_file:
         env_file.write('cache_folderpath = ok/cache')
-    assert local_cacheasy.get_cache_folderpath() == os.path.join('ok', 'cache')
+    assert cacheasy.get_cache_folderpath() == os.path.join('ok', 'cache')
 
 
 def test_cache_filepath():
-    assert local_cacheasy.get_cache_filepath('a') == os.path.join('cache', 'a')
+    assert cacheasy.get_cache_filepath('a') == os.path.join('cache', 'a')
+
+
+def test_write_cache():
+    x = 5
+    cache_file_path = 'my_cool_x'
+    cacheasy.write_to_cache(x, cache_file_path)
+    assert os.path.isfile(os.path.join('cache', cache_file_path))
+    cacheasy.remove_from_cache(cache_file_path)
+    assert not cacheasy.file_in_cache(cache_file_path)
+
+
+def test_write_cache_with_env(env_file_path):
+    with open(env_file_path, 'w') as env_file:
+        env_file.write('cache_folderpath = a/b/c')
+    x = 5
+    cache_file_path = 'my_cool_x'
+    cacheasy.write_to_cache(x, cache_file_path)
+    assert os.path.isfile(os.path.join('a', 'b', 'c', cache_file_path))
+    cacheasy.remove_from_cache(cache_file_path)
+    assert not cacheasy.file_in_cache(cache_file_path)
+
+    shutil.rmtree('a')
+
+
+def test_read(my_cool_x_cached_file_name):
+    y = cacheasy.read_from_cache(my_cool_x_cached_file_name)
+    assert y == 5
+
+
+def test_clear(my_cool_x_cached_file_name):
+    cacheasy.clear_cache()
+    assert not os.path.isdir(cacheasy.get_cache_folderpath())
